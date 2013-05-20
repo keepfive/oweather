b2e31918a4dbeadf0765d360a33f9dcb5494eaf1
Vyacheslav Boychenko <massive.kinetics@gmail.com>
2013-05-20 14:18:53 +0300

    Widget updating

--- src/com/massivekinetics/ow/data/manager/WeatherDataManager.java
+++ src/com/massivekinetics/ow/data/manager/WeatherDataManager.java
@@ -1,5 +1,9 @@
 package com.massivekinetics.ow.data.manager;
 
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import com.massivekinetics.ow.R;
 import com.massivekinetics.ow.application.OWApplication;
@@ -9,6 +13,7 @@ import com.massivekinetics.ow.data.model.WeatherModel;
 import com.massivekinetics.ow.data.tasks.GetWeatherTask;
 import com.massivekinetics.ow.data.tasks.LoadingListener;
 import com.massivekinetics.ow.utils.DateUtils;
+import com.massivekinetics.ow.widgets.oWeatherProvider4x1;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -67,6 +72,7 @@ public class WeatherDataManager implements DataManager {
     public void updateForecast(WeatherForecast forecast) {
         this.mWeatherForecast = forecast;
         saveForecast();
+        updateWidget(OWApplication.getInstance());
     }
 
     @Override
@@ -149,5 +155,14 @@ public class WeatherDataManager implements DataManager {
 
         return notification;
 
+    }
+
+    private void updateWidget(Context context){
+        AppWidgetManager man = AppWidgetManager.getInstance(context);
+        int[] ids = man.getAppWidgetIds(new ComponentName(context,oWeatherProvider4x1.class));
+        Intent updateIntent = new Intent();
+        updateIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        updateIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
+        context.sendBroadcast(updateIntent);
     }
 }
--- src/com/massivekinetics/ow/widgets/oWeatherProvider4x1.java
+++ src/com/massivekinetics/ow/widgets/oWeatherProvider4x1.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.widget.RemoteViews;
 import com.massivekinetics.ow.R;
+import com.massivekinetics.ow.activities.UpdatePageActivity;
 import com.massivekinetics.ow.application.OWApplication;
 import com.massivekinetics.ow.data.model.WeatherModel;
 import com.massivekinetics.ow.states.WeatherState;
@@ -20,17 +21,15 @@ import java.util.Date;
 
 public class oWeatherProvider4x1 extends AppWidgetProvider {
 
-	public static final String ACTION = "android.appwidget.action.APPWIDGET_UPDATE";
+    public static final String ACTION = "android.appwidget.action.APPWIDGET_UPDATE";
 
-
-	@Override
-	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-			int[] appWidgetIds) {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,  int[] appWidgetIds) {
         SimpleDateFormat dataFormatter = new SimpleDateFormat("dd.MM.yyyy");
-		// Get all ids
-		ComponentName thisWidget = new ComponentName(context, oWeatherProvider4x1.class);
-		int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
-		for (int widgetId : allWidgetIds) {
+        // Get all ids
+        ComponentName thisWidget = new ComponentName(context, oWeatherProvider4x1.class);
+        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+        for (int widgetId : allWidgetIds) {
 
             WeatherModel current = OWApplication.getInstance().getDataManager().getCurrentWeather();
 
@@ -42,7 +41,9 @@ public class oWeatherProvider4x1 extends AppWidgetProvider {
             int backgroundColor = R.color.ow_sunny;
             int weatherImage = R.drawable.weather_state_1;
 
-            if(current != WeatherModel.NULL){
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),  R.layout.widget_img_4x1);
+
+            if (current != WeatherModel.NULL) {
                 cityStr = (cityStr == null) ? "Unknown" : cityStr;
                 weatherDescStr = current.getState().getValue();
                 currentTemp = current.getMaxTemperature();
@@ -50,42 +51,54 @@ public class oWeatherProvider4x1 extends AppWidgetProvider {
                 WeatherState weatherState = current.getState();
                 backgroundColor = ResourcesCodeUtils.getWeatherBackgroundColor(weatherState);
                 weatherImage = ResourcesCodeUtils.getWeatherImageResource(weatherState);
+
+                Bitmap city = BitmapUtils.getStringAsBitmap(cityStr, 40, 4, 35, 11);
+
+                Bitmap weatherDesc = BitmapUtils.getStringAsBitmap(weatherDescStr, 28, 10, 32, -1);
+                Bitmap night = BitmapUtils.getStringAsBitmapWithSize("Night", 24, 0, 23, -1, 40, 20);
+
+                int xOffset = nightTempStr.length() > 1 ? 5 : 10;
+                Bitmap nightTemp = BitmapUtils.getStringAsBitmapWithSize(nightTempStr, 27, xOffset, 29, -1, 40, 20);
+
+                xOffset = currentTemp.length() > 1 ? 7 : 60;
+                xOffset = currentTemp.length() > 2 ? -27 : xOffset;
+                int textSize = currentTemp.length() > 2 ? 102 : 110;
+                Bitmap temp = BitmapUtils.getStringAsBitmap2(currentTemp, 102, xOffset, 88);
+
+                remoteViews.setImageViewBitmap(R.id.ivCity, city);
+                remoteViews.setImageViewBitmap(R.id.ivWeatherDesc, weatherDesc);
+                remoteViews.setImageViewBitmap(R.id.ivTemp, temp);
+                remoteViews.setImageViewBitmap(R.id.ivDaytime, night);
+                remoteViews.setImageViewBitmap(R.id.ivNightTemp, nightTemp);
+                remoteViews.setImageViewResource(R.id.ivWeather, weatherImage);
+                remoteViews.setImageViewResource(R.id.ivDegrees, R.drawable.degrees);
+                //remoteViews.setTextViewText(R.id.tvCity, cities[ran]);
+
+
+            } else {
+                backgroundColor = R.color.background;
+                Bitmap city = BitmapUtils.getStringAsBitmap("oW : No data", 40, 4, 35, 13);
+
+                remoteViews.setImageViewBitmap(R.id.ivCity, city);
+                remoteViews.setImageViewResource(R.id.ivTemp, R.drawable.icon);
+
+                remoteViews.setImageViewResource(R.id.ivWeatherDesc, R.color.transparent);
+                remoteViews.setImageViewResource(R.id.ivTemp,  R.color.transparent);
+                remoteViews.setImageViewResource(R.id.ivDaytime,  R.color.transparent);
+                remoteViews.setImageViewResource(R.id.ivNightTemp,  R.color.transparent);
+                remoteViews.setImageViewResource(R.id.ivWeather,  R.color.transparent);
+                remoteViews.setImageViewResource(R.id.ivDegrees,  R.color.transparent);
+
             }
 
-			RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
-					R.layout.widget_img_4x1);
-
-            Bitmap city = BitmapUtils.getStringAsBitmap(cityStr, 40, 4, 35, 11);
-            Bitmap date =  BitmapUtils.getStringAsBitmap(todayDate, 20, 5, 28, -1);
-            Bitmap weatherDesc = BitmapUtils.getStringAsBitmap(weatherDescStr, 28, 10, 32, -1);
-            Bitmap night = BitmapUtils.getStringAsBitmapWithSize("Night", 24, 0, 23, -1, 40, 20);
-
-            int xOffset = nightTempStr.length() > 1 ? 5 : 10;
-            Bitmap nightTemp = BitmapUtils.getStringAsBitmapWithSize(nightTempStr, 27, xOffset, 29, -1, 40, 20);
-
-            xOffset = currentTemp.length() > 1 ? 7 : 60;
-            xOffset = currentTemp.length() > 2 ? -27 : xOffset;
-            int textSize = currentTemp.length() > 2 ? 102 : 110;
-            Bitmap temp = BitmapUtils.getStringAsBitmap2(currentTemp, 102, xOffset, 88);
-
-            remoteViews.setImageViewBitmap(R.id.ivCity, city);
+            Bitmap date = BitmapUtils.getStringAsBitmap(todayDate, 20, 5, 28, -1);
             remoteViews.setImageViewBitmap(R.id.ivDate, date);
-            remoteViews.setImageViewBitmap(R.id.ivWeatherDesc, weatherDesc);
-            remoteViews.setImageViewBitmap(R.id.ivTemp, temp);
-            remoteViews.setImageViewBitmap(R.id.ivDaytime, night);
-            remoteViews.setImageViewBitmap(R.id.ivNightTemp, nightTemp);
-            remoteViews.setImageViewResource(R.id.ivWeather, weatherImage);
             remoteViews.setImageViewResource(R.id.background, backgroundColor);
-            //remoteViews.setTextViewText(R.id.tvCity, cities[ran]);
 
-            Intent intent = new Intent(context, oWeatherProvider4x1.class);
-
-			intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-			intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
-
-			PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-			remoteViews.setOnClickPendingIntent(R.id.main, pendingIntent);
-			appWidgetManager.updateAppWidget(widgetId, remoteViews);
-		}
-	}
+            Intent configIntent = new Intent(context, UpdatePageActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, configIntent, 0);
+            remoteViews.setOnClickPendingIntent(R.id.main, pendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
+        }
+    }
 }
